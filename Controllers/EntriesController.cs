1bfaced465c936c804fd14ffed6c01bfdfc591a2
Charles Morris <morrissoftware1229@gmail.com>
2023-02-09 16:24:48 -0600

    Added Entries controller and its related views

---
+++ Controllers/EntriesController.cs
@@ -0,0 +1,37 @@
+﻿using CalorieCountingApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalorieCountingApplication.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private readonly CalorieCountingApplicationContext _context;
+
+        public EntriesController(CalorieCountingApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult AddItem()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> PostFoodItem([Bind("FoodEntryId,Food,Amount,CalorieCount,DateTime")] FoodEntry foodEntry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(foodEntry);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(AddItem));
+            }
+            return View(foodEntry);
+        }
+
+        public async Task<IActionResult> SeeDailyLog()
+        {
+            return View(await _context.FoodEntries.ToListAsync());
+        }
+    }
+}
---
+++ ViewModels/Entries.cs
@@ -0,0 +1,10 @@
+﻿using CalorieCountingApplication.Models;
+
+namespace CalorieCountingApplication.ViewModels
+{
+    public class Entries
+    {
+        public ExerciseEntry ExerciseEntry { get; set; }
+        public FoodEntry FoodEntry { get; set; }
+    }
+}
